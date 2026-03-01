773727b54c3365823d95e47c07bec1dec07d9ded
Retrod18 <154795523+Retrod18@users.noreply.github.com>
2026-03-01 21:03:14 +0530

    Add manualChunks configuration to Vite build
    Configure manual chunking for vendor libraries in build.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) {
+              return 'vendor-framer';
+            }
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor-react';
+            }
+            if (id.includes('firebase')) {
+              return 'vendor-firebase';
+            }
+            return 'vendor'; // all other package goes here
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
